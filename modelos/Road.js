85ae89f4034bd5365feafe50ba89ad1441d5a92a
juancano37192-cpu <juan.cano37192@ucaldas.edu.co>
2026-03-05 10:40:35 -0500

    Creacion Clase Road

---
+++ modelos/Road.js
@@ -0,0 +1,11 @@
+class Road{
+    constructor(){
+        this._cost = cost;
+    }
+
+    // Getters
+    getCost() { return this._cost; }
+
+    // Setters
+    setCost(cost) { this._cost = cost; }
+}
