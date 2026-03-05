8835fbe90e22889356fd34f88d70fe5b548af3e0
juancano37192-cpu <juan.cano37192@ucaldas.edu.co>
2026-03-05 10:48:38 -0500

    Creacion Clase Building

---
+++ modelos/Building.js
@@ -0,0 +1,26 @@
+class Building {
+    constructor(){
+        this._id = id;
+        this._name = name;
+        this._cost = cookieStore;
+        this._maintenanceCost = maintenanceCost;
+        this._electricityConsumption = electricityConsumption;
+        this._waterConsumption = waterConsumption;
+    }
+
+    // Getters
+    getId() { return this._id; }
+    getName() { return this._name; }
+    getCost() { return this._cost; }
+    getMaintenanceCost() { return this._maintenanceCost; }
+    getElectricityConsumption() { return this._electricityConsumption; }
+    getWaterConsumption() { return this._waterConsumption; }
+
+    // Setters
+    setId(id) { this._id = id; }
+    setName(name) { this._name = name; }
+    setCost(cost) { this._cost = cost; }
+    setMaintenanceCost(maintenanceCost) { this._maintenanceCost = maintenanceCost; }
+    setElectricityConsumption(electricityConsumption) { this._electricityConsumption = electricityConsumption; }
+    setWaterConsumption(waterConsumption) { this._waterConsumption = waterConsumption; }
+}
