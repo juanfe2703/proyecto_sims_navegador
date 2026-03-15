a1b1ddd0e1408c61529dffeeeb1a8a82f5a50e79
juanfe2703 <juanfelipecalderonladino@gmail.com>
2026-03-15 11:28:27 -0500

    este commit es para reportar actualizacion en el archivo de CitizenService, el cual fue mejorado para poder crear ciudadanos, asignarles casa y trabajo

--- negocio/CitizenService.js
+++ negocio/CitizenService.js
@@ -1,15 +1,97 @@
 class CitizenService {
-    assignHousing(city) {
-        // lógica futura
-    }
-
-    assignJobs(city) {
-        // lógica futura
-    }
-
+    //actualización citizen. ahora deberia devolver al ciudadano creado y tambien 
+    //podre agregarle luego una casa y un trabajo
     createCitizen(city) {
+
         const id = city.getCitizens().length + 1
+
         const citizen = new Citizen(id)
+
         city.addCitizen(citizen)
+
+        return citizen
     }
+
+
+
+
+    //asignar casita
+    assignHousing(city){
+        //with this i search citizens
+        const citizens = city.getCitizens()
+
+        //with this i search residential buildings
+        const residentialBuildings = city.getBuildings().filter(
+            b => b.type === "residential"
+        )
+
+        //for eachc to see if the citizen have house, then find an empty house and finally give him
+        citizens.forEach(citizen => {
+
+            if(!citizen.home){
+
+                const house = residentialBuildings.find(
+                    building => building.hasSpace()
+                )
+
+                if(house){
+
+                    house.addResident(citizen)
+
+                    citizen.home = house
+
+                }
+
+            }
+
+        })
+
+    }
+
+
+
+
+    assignJobs(city){
+        //got citizens
+        const citizens = city.getCitizens()
+
+        //search buildings with jobs
+        const workplaces = city.getBuildings().filter(
+            b => b.type === "commercial" || b.type === "industrial"
+        )
+
+        //see if any citizen doesnt have job and find an empty job to asigned him
+        citizens.forEach(citizen => {
+
+            if(!citizen.job){
+
+                const job = workplaces.find(
+                    workplace => workplace.hasVacancy()
+                )
+
+                if(job){
+
+                    job.addWorker(citizen)
+
+                    citizen.job = job
+
+                }
+
+            }
+
+        })
+
+    }
+
+
+    //this method will execute all
+     processCitizens(city){
+
+        this.assignHousing(city)
+
+        this.assignJobs(city)
+
+    }
+
+    
 }
