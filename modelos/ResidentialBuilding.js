a327426b711604d61ce94896915652bceaf66f63
juanfe2703 <juanfelipecalderonladino@gmail.com>
2026-03-15 11:36:13 -0500

    correccion en ResidentialBuilding, especificamente el constructor

--- modelos/ResidentialBuilding.js
+++ modelos/ResidentialBuilding.js
@@ -1,6 +1,6 @@
 class ResidentialBuilding extends Building {
-    constructor(){
-        super();
+    constructor(id, name, cost, maintenanceCost, electricityConsumption, waterConsumption, capacity){
+        super(id, name, cost, maintenanceCost, electricityConsumption, waterConsumption);
         this._capacity = capacity;
         this._citizens = [];
     }
@@ -8,7 +8,7 @@ class ResidentialBuilding extends Building {
     // Getters
     getCapacity() { return this._capacity; }
     getCitizens() { return this._citizens; }
-
+ 
     // Setters
     setCapacity(capacity) { this._capacity = capacity; }
     setCitizens(citizens) { this._citizens = citizens; }
