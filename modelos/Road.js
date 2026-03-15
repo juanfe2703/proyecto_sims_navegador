ca412e47b04fb43bcef4281b08fcb52de459ceb1
juanfe2703 <juanfelipecalderonladino@gmail.com>
2026-03-15 13:15:12 -0500

    corregido constructor de road.js

--- modelos/Road.js
+++ modelos/Road.js
@@ -1,11 +1,11 @@
 class Road{
-    constructor(){
+    constructor(cost = 100){
         this._cost = cost;
     }
-
+ 
     // Getters
     getCost() { return this._cost; }
-
+ 
     // Setters
     setCost(cost) { this._cost = cost; }
 }
